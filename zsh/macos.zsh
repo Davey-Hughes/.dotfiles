dbf843fcb9fee9d48d681697398208713ac9dd90
davey <davidralphhughes@college.harvard.edu>
2018-06-11 23:26:00 -0600

    Moves gobin path to main .zshrc

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,4 +1,4 @@
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8:$HOME/go/bin"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8"
 
 alias gcc="gcc-8"
 alias g++="g++-8"
