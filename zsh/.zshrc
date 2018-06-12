4a57305a0804e501b4786989cecf8082df75a6a5
davey <davidralphhughes@college.harvard.edu>
2018-06-11 21:57:47 -0600

    Adds gopath information

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -37,6 +37,8 @@ setopt interactivecomments
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
 export LANG="en_US.UTF-8"
 
+export GOPATH="$HOME/go"
+
 source $ZSH/oh-my-zsh.sh
 fpath=(/usr/local/share/zsh-completions $fpath)
 
--- zsh/ubuntu.zsh
+++ zsh/ubuntu.zsh
@@ -1,3 +1,5 @@
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games/:$HOME/.local/bin"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games/:$HOME/.local/bin:/$HOME/go/bin"
 
 TZ='America/Denver'; export TZ
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
