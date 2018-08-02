139d455f83faa22a0b116824abfcceb8033ece9f
davey <davidralphhughes@college.harvard.edu>
2018-08-02 09:26:18 -0600

    Adds coreutils to PATH so 'g' prefix not used

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,4 +1,4 @@
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8:$PATH"
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8:/usr/local/opt/coreutils/libexec/gnubin:$PATH"
 
 alias gcc="gcc-8"
 alias g++="g++-8"
