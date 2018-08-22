1495279f6c99371fd9a3e8140ff8a6f88477246f
davey <davidralphhughes@college.harvard.edu>
2018-08-22 01:39:22 -0600

    Adds vi alias for zsh

--- zsh/ubuntu.zsh
+++ zsh/ubuntu.zsh
@@ -3,3 +3,5 @@ export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/g
 TZ='America/Denver'; export TZ
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+alias vi="vim -v -u NONE"
