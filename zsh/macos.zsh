253d11956676ee2885b6247ec5fd5e79f8995387
davey <davidralphhughes@college.harvard.edu>
2018-09-20 16:57:13 -0400

    Edits PATH ordering in macos.zsh

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,4 +1,4 @@
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8:/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/bin:$HOME/Documents/macvim/src/MacVim:/Library/TeX/texbin:/usr/local/opt/llvm/bin:/bin/flake8:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/llvm/bin/:$PATH"
 
 alias gcc="gcc-8"
 alias g++="g++-8"
