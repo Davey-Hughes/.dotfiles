b9b8cb144e213687e7d68a2569c0017f422d7f16
davey <davidralphhughes@college.harvard.edu>
2018-10-09 05:15:05 -0400

    Adds .psqlrc

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ ln -s $DOTFDIR/zsh/.zshrc $HOME
 ln -s $DOTFDIR/tmux/.tmux.conf $HOME
 ln -s $DOTFDIR/readline/.inputrc $HOME
 ln -s $DOTFDIR/readline/.editrc $HOME
+ln -s $DOTFDIR/postgres/.psqlrc $HOME
 ln -s $DOTFDIR/.git_template $HOME
 ln -s $DOTFDIR/powerline $HOME/.config
