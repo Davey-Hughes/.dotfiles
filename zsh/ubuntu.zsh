222204ceec268a81bc6a0c2179750f4744def92d
davey <davidralphhughes@college.harvard.edu>
2019-02-27 21:54:58 -0500

    Adds stderred LD_PRELOAD

--- zsh/ubuntu.zsh
+++ zsh/ubuntu.zsh
@@ -1,5 +1,7 @@
 export PATH="$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games/:$HOME/.local/bin"
 
+export LD_PRELOAD="$HOME/.local/sources/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+
 TZ='America/New_York'; export TZ
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
