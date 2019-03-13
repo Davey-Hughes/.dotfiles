8f76ec1676f874ddc2bb95d23b0468825f9e802d
davey <davidralphhughes@college.harvard.edu>
2019-03-13 02:59:27 -0400

    Adds .local to C/C++ include path

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -1,5 +1,8 @@
 export PATH="$PATH:$HOME/.local/bin"
 
+export C_INCLUDE_PATH="$HOME/.local/include:$C_INCLUDE_PATH"
+export CPLUS_INCLUDE_PATH="$HOME/.local/include:$CPLUS_INCLUDE_PATH"
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 alias vi="vim -v -u NONE"
