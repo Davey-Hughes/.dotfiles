7ab4283cacf8e35ff41693907f92babe17875c70
davey <davidralphhughes@college.harvard.edu>
2021-05-15 06:23:48 -0700

    Adds stderred to arch

--- zsh/arch.zsh
+++ zsh/arch.zsh
@@ -3,6 +3,9 @@ export PATH="$PATH:$HOME/.local/bin"
 export C_INCLUDE_PATH="$HOME/.local/include:$C_INCLUDE_PATH"
 export CPLUS_INCLUDE_PATH="$HOME/.local/include:$CPLUS_INCLUDE_PATH"
 
+# prints stderr in red
+export LD_PRELOAD="$HOME/.local/sources/stderred/build/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 alias vi="vim -v -u NONE"
