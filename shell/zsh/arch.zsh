3c66472f76960ffc32e7b6a392c18c8a92b0fe3e
Davey Hughes <davidralphhughes@gmail.com>
2023-07-30 07:18:35 -0700

    Add pamac package list and zsh/kitty/tmux config changes

--- shell/zsh/arch.zsh
+++ shell/zsh/arch.zsh
@@ -2,3 +2,5 @@ export PATH="$PATH:$HOME/.local/bin"
 
 export C_INCLUDE_PATH="$HOME/.local/include:$C_INCLUDE_PATH"
 export CPLUS_INCLUDE_PATH="$HOME/.local/include:$CPLUS_INCLUDE_PATH"
+
+export QSYS_ROOTDIR="/var/tmp/pamac-build-davey/quartus-free/pkg/quartus-free-quartus/opt/intelFPGA/21.1/quartus/sopc_builder/bin"
