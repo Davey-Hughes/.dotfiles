8882df3ba991f6fe4491c046e41dc54783295956
David Hughes <dhughes@flexport.com>
2023-09-19 11:56:22 -0700

    Add yabai config

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -3,7 +3,7 @@ set -g fish_greeting
 
 set -x SHELL /usr/bin/fish
 
-set PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games $HOME/go/bin
+set PATH /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin /usr/games $HOME/go/bin $HOME/.cargo/bin
 set GOPATH $HOME/go
 
 # vi keybinds
@@ -26,27 +26,27 @@ function fish_user_key_bindings
 end
 
 switch $(uname)
-case Linux
-    source $HOME/.dotfiles/shell/fish/linux.fish
+    case Linux
+        source $HOME/.dotfiles/shell/fish/linux.fish
 
-    set linux_version $(cat /proc/version)
-    switch $linux_version
-    case "*arch*"
-        source $HOME/.dotfiles/shell/fish/arch.fish
-        set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/arch.conf
-    case "*MANJARO*"
-        source $HOME/.dotfiles/shell/fish/manjaro.fish
-        set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/manjaro.conf
-    end
+        set linux_version $(cat /proc/version)
+        switch $linux_version
+            case "*arch*"
+                source $HOME/.dotfiles/shell/fish/arch.fish
+                set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/arch.conf
+            case "*MANJARO*"
+                source $HOME/.dotfiles/shell/fish/manjaro.fish
+                set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/manjaro.conf
+        end
 
-case Darwin
-    if [ $hostname = "dhughes-K44H0465N7-mbp" ]
-        source $HOME/.dotfiles/shell/fish/macos_flexport.fish
-        set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/macos_flexport.conf
-    else
-        source $HOME/.dotfiles/shell/fish/macos.fish
-        set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/macos.conf
-    end
+    case Darwin
+        if [ $hostname = dhughes-K44H0465N7-mbp ]
+            source $HOME/.dotfiles/shell/fish/macos_flexport.fish
+            set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/macos_flexport.conf
+        else
+            source $HOME/.dotfiles/shell/fish/macos.fish
+            set -x TMUXCONFIG $HOME/.dotfiles/term/tmux/macos.conf
+        end
 end
 
 # set EDITOR to neovim if exists
@@ -62,32 +62,32 @@ if status is-interactive
     set kitty_theme "Tokyo Night Moon"
 
     switch $COLORSCHEME
-    case tokyonight
-        switch $COLORSCHEME_VARIANT
-        case night
-            set kitty_theme "Tokyo Night"
-        case storm
-            set kitty_theme "Tokyo Night Storm"
-        case day
-            set kitty_theme "Tokyo Night Day"
-        case '*'
-            set kitty_theme "Tokyo Night Moon"
-        end
+        case tokyonight
+            switch $COLORSCHEME_VARIANT
+                case night
+                    set kitty_theme "Tokyo Night"
+                case storm
+                    set kitty_theme "Tokyo Night Storm"
+                case day
+                    set kitty_theme "Tokyo Night Day"
+                case '*'
+                    set kitty_theme "Tokyo Night Moon"
+            end
 
-    case neosolarized
-        switch $COLORSCHEME_VARIANT
-        case light
-            set kitty_theme "Solarized Light"
-        case '*'
-            set kitty_theme "Solarized Dark - Patched"
-        end
+        case neosolarized
+            switch $COLORSCHEME_VARIANT
+                case light
+                    set kitty_theme "Solarized Light"
+                case '*'
+                    set kitty_theme "Solarized Dark - Patched"
+            end
     end
 
     kitty +kitten themes --reload-in=all $kitty_theme
 
     # open or attach tmux in session 'main' if no arguments are passed
     function tmux
-        if count $argv > /dev/null
+        if count $argv >/dev/null
             command tmux $argv
         else
             command tmux new-session -A -s main
