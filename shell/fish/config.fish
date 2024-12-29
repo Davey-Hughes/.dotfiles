5e422511a23accfc7e0d6bb63681ea21ff30c0f8
Davey Hughes <davidralphhughes@gmail.com>
2024-12-29 01:41:21 -0800

    Set cursor to block in insert mode

--- shell/fish/config.fish
+++ shell/fish/config.fish
@@ -52,6 +52,9 @@ if command -q nvim
 end
 
 if status is-interactive
+    set -g fish_cursor_default block
+    set -g fish_cursor_insert block
+
     set -g fish_escape_delay_ms 10
 
     set -x COLORSCHEME tokyonight
