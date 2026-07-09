d3f8e2c49e185dacf956d92a61e1b95fb52625bf
Davey Hughes <davidralphhughes@gmail.com>
2026-07-09 01:40:42 -0700

    Refactor and simplify install.sh

--- install.sh
+++ install.sh
@@ -1,53 +1,6 @@
 #!/bin/bash
 
-usage() {
-  cat <<EOF
-usage: $0 [-h] [-z] [-f] [-p] [-v] [-w]
-    -h: print this usage statement
-    -b: install homebrew
-    -z: install oh-my-zsh
-    -p: install packages
-    -v: install vim
-EOF
-
-  exit 1
-}
-
-install_homebrew() {
-  echo "Installing homebrew..."
-
-  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-}
-
-install_ohmyzsh() {
-  echo "Installing oh-my-zsh..."
-
-  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-}
-
-install_packages() {
-  if [ "$(command -v brew)" ]; then
-    echo "Installing homebrew packages..."
-
-    brew bundle --no-lock --file $DOTFDIR/os/$KERNEL/homebrew/Brewfile
-  else
-    echo "homebrew not installed" 2>&1
-  fi
-
-  if [ "$(command -v pip3)" ]; then
-    echo "Installing pip3 packages..."
-
-    pip3 install -r $DOTFDIR/pip/requirements.txt
-  else
-    echo "pip3 not installed" 2>&1
-  fi
-}
-
-install_vim() {
-  pushd $HOME/.vim
-  ./install.sh
-  popd
-}
+DOTFDIR=$HOME/.dotfiles
 
 symlinks() {
   echo "Creating symlinks..."
@@ -57,119 +10,51 @@ symlinks() {
     exit 1
   fi
 
+  # Create base directories
   mkdir -p $HOME/.tmux
   mkdir -p $HOME/.config
 
-  # ln -sfn $DOTFDIR/zsh/.zshrc $HOME
-  # ln -sfn $DOTFDIR/zsh/davey.zsh-theme $HOME/.oh-my-zsh/themes
-  #
+  # Custom cross-linking
   ln -sfn $HOME/.vim $HOME/.config/nvim
 
-  pushd $DOTFDIR
+  pushd $DOTFDIR >/dev/null
   stow -t $HOME/.config config
   stow -t $HOME home
-  popd
-
+  popd >/dev/null
 }
 
 git_settings() {
   echo "Applying git settings..."
 
   if [ "$(command -v git)" ]; then
-    git config --global init.templatedir '~/.git_template'
+    git config --global init.templatedir "$HOME/.git_template"
     git config --global alias.ctags '!.git/hooks/ctags'
     git config --global init.defaultBranch 'main'
-    git config --global core.excludesfile '.git_template/.gitignore'
+    git config --global core.excludesfile "$HOME/.git_template/.gitignore"
     git config --global push.autoSetupRemote true
   else
-    echo "git not installed" 2 &>1
+    echo "git not installed" 2>&1
   fi
 }
 
-# assumes dotfiles directory is at this location
-DOTFDIR=$HOME/.dotfiles
+install_packages() {
+  if [ "$(command -v brew)" ]; then
+    KERNEL="linux"
+    if [ "$(uname)" = "Darwin" ]; then
+      KERNEL="macos"
+    fi
 
-INSTALL_HOMEBREW=false
-INSTALL_OMZSH=false
-INSTALL_OMFISH=false
-INSTALL_PACKAGES=false
-INSTALL_VIM=false
-
-# these always run by default
-CREATE_SYMLINKS=true
-GIT_SETTINGS=true
-
-while getopts "hbzfpwv" opt; do
-  case "${opt}" in
-  b)
-    INSTALL_HOMEBREW=true
-    ;;
-  z)
-    INSTALL_OMZSH=true
-    ;;
-  f)
-    INSTALL_OMFISH=true
-    ;;
-  p)
-    INSTALL_PACKAGES=true
-    ;;
-  v)
-    INSTALL_VIM=true
-    ;;
-  h | *)
-    usage
-    ;;
-  esac
-done
-
-KERNEL=''
-OS=''
-
-case $(uname) in
-Darwin)
-  KERNEL='macos'
-  ;;
-Linux)
-  KERNEL='linux'
-
-  version=$(cat /proc/version)
-  if [[ $version =~ "arch" ]]; then
-    :
-  elif [[ $version =~ "Red Hat" ]]; then
-    :
-  elif [[ $version =~ "WSL2" ]]; then
-    :
-  else
-    :
+    BREWFILE="$DOTFDIR/os/$KERNEL/homebrew/Brewfile"
+    if [ -f "$BREWFILE" ]; then
+      echo "Installing homebrew packages..."
+      brew bundle --no-lock --file "$BREWFILE"
+    fi
   fi
-  ;;
-FreeBSD) ;;
-esac
+}
 
-if [ "$INSTALL_HOMEBREW" = true ]; then
-  install_homebrew
-fi
+# Run the core setup tasks automatically
+symlinks
+git_settings
+install_packages
 
-if [ "$INSTALL_OMZSH" = true ]; then
-  install_ohmyzsh
-fi
-
-if [ "$INSTALL_OMFISH" = true ]; then
-  install_ohmyfish
-fi
-
-if [ "$INSTALL_PACKAGES" = true ]; then
-  install_packages
-fi
-
-if [ "$INSTALL_VIM" = true ]; then
-  install_vim
-fi
-
-if [ "$CREATE_SYMLINKS" = true ]; then
-  symlinks
-fi
-
-if [ "$GIT_SETTINGS" = true ]; then
-  git_settings
-fi
+echo "Dotfiles installation complete!"
